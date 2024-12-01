d81805122108bd65be16ac1f706fb610ae7593fe
Kadir Ince <kadirince@cormind.com>
2024-12-01 16:18:45 +0300

    Update TypeScript build info and enhance Vite configuration with server and preview settings

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-    plugins: [react()],
-    server: {},
-    base: "/",
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+    historyApiFallback: true
+  },
+  base: "/",
+  preview: {
+    port: 3000,
+    open: true,
+    historyApiFallback: true
+  }
 });
